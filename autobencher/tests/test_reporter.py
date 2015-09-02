3c6816900f17cee8e56cbc209445c4a2c3ed1265
Anders Pitman <tapitman11@gmail.com>
2015-09-01 19:24:05 -0700

    Fix relative imports for tests

--- autobencher/tests/test_reporter.py
+++ autobencher/tests/test_reporter.py
@@ -1,13 +1,13 @@
 import sys, os
 myPath = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, myPath + '/../')
+sys.path.insert(0, myPath + '/../../')
 
 from unittest import mock
 
-from reporter import ASVBenchmarkReporter
-from util import Authorization
+from autobencher.reporter import ASVBenchmarkReporter
+from autobencher.util import Authorization
 
-@mock.patch('reporter.requests')
+@mock.patch('autobencher.reporter.requests')
 def test_report_run_started(mock_requests):
     result_uri = 'localhost/results'
     report_uri = 'localhost/report'
@@ -23,8 +23,8 @@ def test_report_run_started(mock_requests):
     mock_requests.post.assert_called_with(report_uri, data=expected_post,
                                           auth=(username, password))
 
-@mock.patch('reporter.check_call')
-@mock.patch('reporter.requests')
+@mock.patch('autobencher.reporter.check_call')
+@mock.patch('autobencher.reporter.requests')
 def test_report_run_finished(mock_requests, mock_check_call):
     result_uri = 'localhost/results'
     report_uri = 'localhost/report'
