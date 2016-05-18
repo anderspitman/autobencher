c24ad97d55013a92397208e75965b66e03af0a5b
Anders Pitman <tapitman11@gmail.com>
2016-05-18 00:25:17 -0700

    Copy results from master before running

--- autobencher/runner.py
+++ autobencher/runner.py
@@ -101,7 +101,6 @@ class ASVMasterProcess(RunnerProcess):
 
         os.chdir(self._branch_dir)
 
-        print(os.getcwd())
         asv_command = ['asv', 'run', 'NEW']
         call(asv_command)
 
@@ -123,6 +122,7 @@ class ASVProcess(RunnerProcess):
         super(ASVProcess, self).__init__(directory, repo_uri)
 
         self._base_commit = repo_base
+        self._results_dir = os.path.join(self._branch_dir, 'results')
 
         self._reporter = reporter
         self._publisher = publisher
@@ -138,16 +138,98 @@ class ASVProcess(RunnerProcess):
 
         os.chdir(self._branch_dir)
 
-        # include 1 previous commit from master so we can see any regressions
-        commit_range = self._base_commit + '~1..' + self._branch_ref
-        asv_command = ['asv', 'run', '--steps', '10', commit_range]
-        check_call(asv_command)
-        self._reporter.report()
+        self._copy_master_results()
+
+        # run for commits after master
+        asv_command = ['asv', 'run', 'NEW']
+        call(asv_command)
+
+        if not self._has_regressions():
+            self._reporter.report_success()
+        else:
+            self._reporter.report_failure()
+
         publish_dest = '/'.join(['pull_requests', self._owner,
                                  self._branch_ref])
         self._publisher.publish(publish_dest)
 
         os.chdir(self._dir)
+
+    def _has_regressions(self):
+        path = self._results_dir
+
+        results_list = list(self._iter_results(path))
+
+        regression = False
+        if len(results_list) > 0:
+
+            # first commit chronologically should be master; last should be the
+            # tip of the branch
+            results_list = sorted(results_list,
+                                  key=lambda result: result['date'])
+            master_commit_hash = self._base_commit
+            tip_commit_hash = results_list[-1]['commit_hash']
+
+            master_results = {}
+            tip_results = {}
+
+            for results in self._iter_results(path):
+                for key, val in results['params'].items():
+                    configuration = \
+                        self._generate_unique_configuration_string(results)
+
+                    if results['commit_hash'] == master_commit_hash:
+                        if configuration not in master_results:
+                            master_results[configuration] = results['results']
+                    elif results['commit_hash'] == tip_commit_hash:
+                        if configuration not in tip_results:
+                            tip_results[configuration] = results['results']
+
+            for configuration in tip_results:
+                for benchmark, value in tip_results[configuration].items():
+                    # if this benchmark exists on master
+                    if benchmark in master_results[configuration]:
+                        master_val = master_results[configuration][benchmark]
+                        tip_val = value
+                        regression = self._is_regression(master_val, tip_val)
+
+                        if regression:
+                            break
+                if regression:
+                    break
+
+        return regression
+
+    # this method was adapted from asv's codebase, in the results.py file
+    def _iter_results(self, results_dir):
+        skip_files = set([
+            'machine.json', 'benchmarks.json'
+        ])
+        for root, dirs, files in os.walk(results_dir):
+            for filename in files:
+                if filename not in skip_files and filename.endswith('.json'):
+                    path = os.path.join(root, filename)
+                    with open(path) as json_file:
+                        data = json.load(json_file)
+                        yield data
+
+    def _generate_unique_configuration_string(self, results):
+        return results['env_name']
+
+    def _is_regression(self, master_val, tip_val):
+        # Detect if more than 1/3 slower
+        return ((tip_val - master_val) / master_val) > .3333
+
+    def _copy_master_results(self):
+        master_results_dir = os.path.join(self._run_dir, 'master',
+                                          'results')
+        self._results_dir = os.path.join(self._branch_dir, 'results')
+        if not os.path.exists(self._results_dir):
+            os.makedirs(self._results_dir)
+
+        copy_command = ['rsync', '-av', master_results_dir,
+                        self._results_dir]
+        check_call(copy_command)
 
     def _set_branch_dir(self):
         self._branch_dir = os.path.join(self._run_dir, self._owner,
